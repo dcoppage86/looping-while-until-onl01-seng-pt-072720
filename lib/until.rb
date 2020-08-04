dfb9e8ab4d9950d0597352856f2ffccb7f3816d1
Dakota Coppage <dakotakcoppage@me.com>
2020-08-04 00:02:09 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
-  until levitaion_force == 10 
-    puts "Wingardium Leviosa"
-    levitation_force += 1 
-  end
+    until levitaion_force == 10 
+      puts "Wingardium Leviosa"
+      levitation_force += 1 
+    end
 end
